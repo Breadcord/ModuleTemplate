423206926af272d51d0eb327e5fb50682d6b189c
Fripe <72686066+Fripe070@users.noreply.github.com>
2024-04-23 20:32:00 +0200

    Move module ID constant

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@ import breadcord
 
 
 class ModuleCogClassName(breadcord.module.ModuleCog):
-    def __init__(self, module_id: str, /):
-        super().__init__(module_id)
+    def __init__(self):
+        super().__init__("module_id")
 
 
 async def setup(bot: breadcord.Bot):
-    await bot.add_cog(ModuleCogClassName("module_id"))
+    await bot.add_cog(ModuleCogClassName())
